21cf343defd18478efaf9153d704103a7106fffe
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2018-04-04 15:20:28 +0200

    ShoppingCart: ES6 destructuring props -- Course React - 4. React Redux https://youtu.be/dAm3jicYvR8?t=8m4s -- Enforce consistent usage of destructuring assignment of props, state, and context (react/destructuring-assignment) https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -10,23 +10,23 @@ const styles = {
 }
 
 
-const ShoppingCart = (props) => {
+const ShoppingCart = ({ cart, removeFromCart }) => {
   return (
     <Panel header="Shopping Cart">
       <Table fill>
         <tbody>
-          {props.cart.map(product =>
+          {cart.map(product =>
             <tr key={product.id}>
               <td>{product.name}</td>
               <td className="text-right">${product.price}</td>
-              <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
+              <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
             </tr>
           )}
         </tbody>
         <tfoot>
           <tr>
             <td colSpan="4" style={styles.footer}>
-              Total: ${props.cart.reduce((sum, product) => sum + product.price, 0)}
+              Total: ${cart.reduce((sum, product) => sum + product.price, 0)}
             </td>
           </tr>
         </tfoot>
