acd0dfef4410193fb38f0bd844f400ea46a594cc
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2018-04-04 15:13:31 +0200

    ShoppingCart as arrow function (const), component presentational, doesn't manage state. -- Course React - 4. React Redux https://youtu.be/dAm3jicYvR8?t=6m52s

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
 import { removeFromCart } from "../actionCreators";
 import { connect } from 'react-redux';
@@ -10,33 +10,31 @@ const styles = {
 }
 
 
-class ShoppingCart extends Component {
-  render() {
-    return (
-      <Panel header="Shopping Cart">
-        <Table fill>
-          <tbody>
-            {this.props.cart.map(product =>
-              <tr key={product.id}>
-                <td>{product.name}</td>
-                <td className="text-right">${product.price}</td>
-                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
-              </tr>
-            )}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="4" style={styles.footer}>
-                Total: ${this.props.cart.reduce((sum, product) => sum + product.price, 0)}
-              </td>
+const ShoppingCart = (props) => {
+  return (
+    <Panel header="Shopping Cart">
+      <Table fill>
+        <tbody>
+          {props.cart.map(product =>
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td className="text-right">${product.price}</td>
+              <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
             </tr>
-          </tfoot>
-        </Table>
+          )}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4" style={styles.footer}>
+              Total: ${props.cart.reduce((sum, product) => sum + product.price, 0)}
+            </td>
+          </tr>
+        </tfoot>
+      </Table>
 
-      </Panel>
-    )
-  }
-}
+    </Panel>
+  );
+};
 
 const mapsStateToProps = state => {
   return {
